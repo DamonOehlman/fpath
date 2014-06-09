2ee9474494f802aea60cbe9af48bf057e5d1fbf2
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-09 16:04:53 +1000

    gendocs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
   The `fpath` module is a simple module that provides some useful helpers when
   working with [pull-streams](https://github.com/dominictarr/pull-streams) and
   the file system.
+
 **/
 
 var fs = require('fs');
@@ -17,14 +18,8 @@ var pull = require('pull-stream');
   A pull-stream source that generates provides the full path of files in the
   specified target path:
 
-  ```js
-  pull(
-    fpath.entries(__dirname),
-    pull.collect(function(err, entries) {
-      console.log('found ' + entries.length + ' entries');
-    })
-  );
-  ```
+  <<< examples/entries.js
+
 **/
 exports.entries = pull.Source(function(targetPath, opts) {
   var files;
@@ -62,50 +57,24 @@ exports.entries = pull.Source(function(targetPath, opts) {
 
   The filter function provides a through stream that will only pass through
   those files that pass a truth test.  As it's usually handy to have more
-  information on the file than just it's name, the `fs.stat` function is 
+  information on the file than just it's name, the `fs.stat` function is
   called on each file before being passed to the test function.
 
   The following example demonstrates how you could only pass through
   directories from an entries source stream:
 
-  ```js
-  pull(
-    fpath.entries(__dirname),
-    fpath.filter(function(filename, stats) {
-      return stats.isDirectory()
-    }),
-    pull.collect(function(err, items) {
-      // items will contain the directory names
-    })
-  );
-  ```
+  <<< examples/filter.js
 
   Additionally, a filter shortcut is available if you just want to call one
   of the many "is*" methods available on an
   [fs.Stats](http://nodejs.org/api/fs.html#fs_class_fs_stats) object:
 
-  ```js
-  pull(
-    fpath.entries(__dirname),
-    fpath.filter('isDirectory'),
-    pull.collect(function(err, items) {
-      // items will contain directory names only
-    })
-  );
-  ```
+  <<< examples/filter-isDirectory.js
 
   In the case when the filter function is called without a test function
   provided all files will be dropped from the stream:
 
-  ```js
-  pull(
-    fpath.entries(__dirname),
-    fpath.filter(),
-    pull.collect(function(err, items) {
-      // no items
-    })
-  );
-  ```
+  <<< examples/filter-empty.js
 **/
 exports.filter = function(test) {
   var pipeline;
